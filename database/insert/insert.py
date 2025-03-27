b6399f1425ab9a22ecadb01c9a5043ee0b945579
FabiantheG <120559364+FabiantheG@users.noreply.github.com>
2025-03-27 10:56:13 +0100

    neu

--- database/insert/insert.py
+++ database/insert/insert.py
@@ -3,6 +3,7 @@
 from database.session import session
 from database.models import *
 
-prov = Provider(name = 'yahoo',provider_id = 997)
+prov = Provider(name = 'yahoo',provider_id = 9997)
 session.add(prov)
 session.commit()
+
