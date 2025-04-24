446db0e6432cddd0fb9452c829bc46750847ad6c
Epicman11 <anderegg98@gmx.ch>
2025-04-24 12:57:20 +0200

    get sim, insert sim , insert_portfolio.py, get_portfolio.py, problem gelöst einruck

--- database/functions/get_simulation.py
+++ database/functions/get_simulation.py
@@ -1,24 +1,24 @@
-    from database.models import *
-    from database.session import session
-    from database.functions.orm_list_to_df import *
+from database.models import *
+from database.session import session
+from database.functions.orm_list_to_df import *
 
 
-    def get_simulation(name, version):
-        sim_name_version = session.query(HEDGING_STRATEGY).filter_by(name=name, version = version).first()
-        if not sim_name_version:
-            print(f"Hedging strategy '{name}' or '{version}' not found.")
-            return
+def get_simulation(name, version):
+    sim_name_version = session.query(HEDGING_STRATEGY).filter_by(name=name, version = version).first()
+    if not sim_name_version:
+        print(f"Hedging strategy '{name}' or '{version}' not found.")
+        return
 
-        else:
-            hedge_id = session.query(HEDGING_STRATEGY.hedge_id).filter_by(name=name, version=version).first()
-            sim_id = session.query(SIMULATION_REF.simulation_id).filter_by(hedge_id= hedge_id).first()
-            new = session.query(SIMULATION_TS).filter_by(simulation_id = sim_id).all()
-            df = orm_list_to_df(new, date_index = False)
-            df = df.drop(columns=["unhedged_growth", "hedged_growth"])
-        return df
+    else:
+        hedge_id = session.query(HEDGING_STRATEGY.hedge_id).filter_by(name=name, version=version).first()
+        sim_id = session.query(SIMULATION_REF.simulation_id).filter_by(hedge_id= hedge_id).first()
+        new = session.query(SIMULATION_TS).filter_by(simulation_id = sim_id).all()
+        df = orm_list_to_df(new, date_index = False)
+        df = df.drop(columns=["unhedged_growth", "hedged_growth"])
+    return df
 
 
 
-    get_simulation('DCF_Hedge',1.2)
+get_simulation('DCF_Hedge',1.2)
 
 
